528db3c7305c8bed86e220729a3a52d31827d84c
CeMuPaMuDa <bepyc9i@gmail.com>
2022-07-13 20:02:23 +0300

    rails base config

--- config/application.rb
+++ config/application.rb
@@ -20,18 +20,10 @@ Bundler.require(*Rails.groups)
 
 module NewsBlog
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
-
-    # Don't generate system test files.
     config.generators.system_tests = nil
+    config.i18n.available_locales = %i[en ru uk]
+    config.i18n.default_locale = :uk
+    config.time_zone = 'Kyiv'
   end
 end
